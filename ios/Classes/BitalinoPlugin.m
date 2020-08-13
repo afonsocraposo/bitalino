352a8a2883bb61f203a450f79c30406a5ece41f1
Afonso Raposo <afonsocraposo@gmail.com>
2020-08-13 23:58:42 +0100

    BITalino IOS working!

--- ios/Classes/BitalinoPlugin.m
+++ ios/Classes/BitalinoPlugin.m
@@ -1,35 +1,63 @@
 #import "BitalinoPlugin.h"
+#import <Flutter/Flutter.h>
 
-#define BITALINO_IDENTIFIER @"DC33B7A6-EA9F-BE9E-47A2-B016CD98CBEE"
+@implementation DataStreamHandler
+- (FlutterError*)onListenWithArguments:(id)arguments eventSink:(FlutterEventSink)eventSink {
+    self.eventSink = eventSink;
+    return nil;
+}
+
+- (FlutterError*)onCancelWithArguments:(id)arguments {
+    self.eventSink = nil;
+    return nil;
+}
+
+- (void) sendFrame:(BITalinoFrame *)frame {
+    if(self.eventSink) {
+        NSMutableDictionary *dataBuffer = [NSMutableDictionary dictionary];
+        dataBuffer[@"sequence"] = [NSNumber numberWithInt:(int)[frame seq]];
+        dataBuffer[@"analog"] = [NSArray arrayWithObjects:[NSNumber numberWithInt:(int)[frame a0]], [NSNumber numberWithInt:(int)[frame a1]], [NSNumber numberWithInt:(int)[frame a2]], [NSNumber numberWithInt:(int)[frame a3]], [NSNumber numberWithInt:(int)[frame a4]], [NSNumber numberWithInt:(int)[frame a5]], nil];
+        dataBuffer[@"digital"] = [NSArray arrayWithObjects:[NSNumber numberWithInt:(int)[frame d0]],[NSNumber numberWithInt:(int)[frame d1]], [NSNumber numberWithInt:(int)[frame d2]], [NSNumber numberWithInt:(int)[frame d3]], nil];
+
+        self.eventSink(dataBuffer);
+    }
+}
+@end
 
 @implementation BitalinoPlugin{
     FlutterResult _connectResult;
-    //FlutterResult _batteryResult;
+    FlutterResult _disconnectResult;
+    FlutterResult _batteryResult;
+    FlutterResult _triggerResult;
+    FlutterResult _startResult;
+    FlutterResult _stopResult;
     FlutterMethodChannel* _channel;
 }
 
-- (instancetype)initWithChannel:(FlutterMethodChannel*)channel
-{
-    self = [super init];
-    if (self) {
-        _channel = channel;
-    }
-    return self;
-}
-
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  FlutterMethodChannel* channel = [FlutterMethodChannel
+    BitalinoPlugin* instance = [[BitalinoPlugin alloc] init];
+    
+    FlutterMethodChannel* channel = [FlutterMethodChannel
       methodChannelWithName:@"com.afonsoraposo.bitalino/bitalino"
             binaryMessenger:[registrar messenger]];
-  BitalinoPlugin* instance = [[BitalinoPlugin alloc] initWithChannel:channel];
-  [registrar addMethodCallDelegate:instance channel:channel];
+    [instance setChannel:channel];
+    [registrar addMethodCallDelegate:instance channel:channel];
+    
+    // Data stream channel
+    instance.dataStreamHandler = [DataStreamHandler new];
+    FlutterEventChannel* dataStreamChannel = [FlutterEventChannel eventChannelWithName:@"com.afonsoraposo.bitalino/dataStream" binaryMessenger:[registrar messenger]];
+    [dataStreamChannel setStreamHandler:instance.dataStreamHandler];
+}
+
+- (void)setChannel:(FlutterMethodChannel*)channel{
+    _channel = channel;
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
   if ([@"initialize" isEqualToString:call.method]) {
-      sampleRate = 1;
       
-      bitalino = [[BITalinoBLE alloc] initWithUUID:BITALINO_IDENTIFIER];
+      NSString* UUID = call.arguments[@"address"];
+      bitalino = [[BITalinoBLE alloc] initWithUUID:UUID];
       bitalino.delegate=self;
       
       result(@YES);
@@ -38,73 +66,131 @@
       @try
       {
           _connectResult = result;
-          if(![bitalino isConnected]){
-              [bitalino scanAndConnect];
-          } else{
-              [bitalino disconnect];
-          }
-          
+          [bitalino scanAndConnect];
       }
       @catch(id exception) {
-          NSLog(@"%@", [exception reason]);
           _connectResult = nil;
           result([FlutterError errorWithCode:@"" message:[exception reason] details:[exception details]]);
       }
-      
+    }else if([@"disconnect" isEqualToString:call.method]) {
+     @try
+      {
+          _disconnectResult = result;
+          [bitalino disconnect];
+      }
+    @catch(id exception) {
+        result([FlutterError errorWithCode:@"" message:[exception reason] details:[exception details]]);
+    }
+     
   }else if([@"version" isEqualToString:call.method]) {
-      result([bitalino version]);
+      @try
+       {
+           result([bitalino version]);
+       }
+     @catch(id exception) {
+         result([FlutterError errorWithCode:@"" message:[exception reason] details:[exception details]]);
+     }
       
-  /*
-   NOT WORKING, CRASHES APP
    }else if([@"batteryThreshold" isEqualToString:call.method]) {
       @try
       {
           _batteryResult = result;
-          NSNumber *threshold = call.arguments[@"threshold"];
-          NSLog(@"%@", threshold);
-          [bitalino setBatteryThreshold:10];
+          NSNumber* threshold = call.arguments[@"threshold"];
+          [bitalino setBatteryThreshold:[threshold intValue]];
        }
        @catch(id exception) {
-           NSLog(@"%@", [exception reason]);
            _batteryResult = nil;
            result([FlutterError errorWithCode:@"" message:[exception reason] details:[exception details]]);
        }
-  */
+       
+   }else if([@"trigger" isEqualToString:call.method]) {
+       @try
+       {
+           _triggerResult = result;
+           NSArray* outputs = call.arguments[@"digitalChannels"];
+           [bitalino setDigitalOutputs:outputs];
+        }
+        @catch(id exception) {
+            _triggerResult = nil;
+            result([FlutterError errorWithCode:@"" message:[exception reason] details:[exception details]]);
+        }
+
+   }else if([@"start" isEqualToString:call.method]) {
+       @try
+       {
+           _startResult = result;
+           NSArray* channels = call.arguments[@"analogChannels"];
+           NSNumber* sampleRate = call.arguments[@"sampleRate"];
+           NSNumber* numberOfSamples = call.arguments[@"numberOfSamples"];
+           
+           [bitalino startRecordingFromAnalogChannels:channels withSampleRate:[sampleRate intValue] numberOfSamples:[numberOfSamples intValue] samplesCompletion:^(BITalinoFrame *frame) {
+               [self->_dataStreamHandler sendFrame:frame];
+           }];
+        }
+        @catch(id exception) {
+            _startResult = nil;
+            result([FlutterError errorWithCode:@"" message:[exception reason] details:[exception details]]);
+        }
+
+   }else if([@"stop" isEqualToString:call.method]) {
+       @try
+       {
+           _stopResult = result;
+          [bitalino stopRecording];
+        }
+        @catch(id exception) {
+            _stopResult  = nil;
+            result([FlutterError errorWithCode:@"" message:[exception reason] details:[exception details]]);
+        }
+
   } else {
     result(FlutterMethodNotImplemented);
   }
 }
 
-
 #pragma BITalino delegates
 -(void)bitalinoDidConnect:(BITalinoBLE *)bitalino{
-    _connectResult(@YES);
-    _connectResult = nil;
+    if(_connectResult!=nil){
+        _connectResult(@YES);
+        _connectResult = nil;
+    }
 }
 
 -(void)bitalinoDidDisconnect:(BITalinoBLE *)bitalino{
-    // disconnect
-    [_channel invokeMethod:@"lostConnection" arguments:nil];
-    NSLog(@"disconnected :(");
+    if(_disconnectResult!=nil){
+        _disconnectResult(@YES);
+        _disconnectResult = nil;
+    }else{
+        [_channel invokeMethod:@"lostConnection" arguments:nil];
+    }
 }
 
 -(void)bitalinoBatteryThresholdUpdated:(BITalinoBLE *)bitalino{
-    /*
-    _batteryResult(@YES);
-    _batteryResult = nil;
-     */
+    if(_batteryResult!=nil){
+        _batteryResult(@YES);
+        _batteryResult = nil;
+    }
 }
 
 -(void)bitalinoBatteryDigitalOutputsUpdated:(BITalinoBLE *)bitalino{
-    //
+    if(_triggerResult){
+        _triggerResult(@YES);
+        _triggerResult = nil;
+    }
 }
 
 -(void)bitalinoRecordingStarted:(BITalinoBLE *)bitalino{
-    //
+    if(_startResult!=nil){
+        _startResult(@YES);
+        _startResult = nil;
+    }
 }
 
 -(void)bitalinoRecordingStopped:(BITalinoBLE *)bitalino{
-    //
+    if(_stopResult!=nil){
+        _stopResult(@YES);
+        _stopResult = nil;
+    }
 }
 
 @end
--- lib/bitalinoException.dart
+++ lib/bitalinoException.dart
@@ -6,7 +6,7 @@ enum BITalinoErrorType {
   BT_DEVICE_NOT_CONNECTED,
   BT_DEVICE_FAILED_CONNECT,
   BT_DEVICE_FAILED_DISCONNECT,
-  DEVICE_NOT_IN_ACQUISITION_MODE,
+  DEVICE_NOT_IN_RECORDING_MODE,
   LOST_CONNECTION,
   BT_DEVICE_ALREADY_CONNECTED,
   CONTROLLER_FAILED_INITIALIZE,
@@ -15,10 +15,16 @@ enum BITalinoErrorType {
   TIMEOUT,
   START_FAILED,
   CUSTOM,
-  BT_DEVICE_ALREADY_ACQUIRING,
-  BT_DEVICE_NOT_ACQUIRING,
+  BT_DEVICE_ALREADY_RECORDING,
+  BT_DEVICE_NOT_RECORDING,
   BT_DEVICE_BTH,
-  NOT_IMPLEMENTED_IOS
+  NOT_IMPLEMENTED_IOS,
+  BAT_THRESHOLD_INVALID,
+  BT_DEVICE_CANNOT_BE_RECORDING,
+  INVALID_DIGITAL_CHANNELS,
+  NOT_BITALINO2,
+  MISSING_PARAMETER,
+  INVALID_ADDRESS
 }
 
 class BITalinoException implements Exception {
@@ -28,7 +34,7 @@ class BITalinoException implements Exception {
   BITalinoException([this.type, this.msg]) {
     switch (type) {
       case BITalinoErrorType.ADDRESS_NULL:
-        msg = "The device address must not be null.";
+        msg = "The device address must not be null nor empty.";
         return;
       case BITalinoErrorType.ALREADY_CONNECTING:
         msg = "A connection attempt is already in process.";
@@ -42,8 +48,8 @@ class BITalinoException implements Exception {
       case BITalinoErrorType.BT_DEVICE_FAILED_DISCONNECT:
         msg = "Failed to disconnect from the bluetooth device.";
         return;
-      case BITalinoErrorType.DEVICE_NOT_IN_ACQUISITION_MODE:
-        msg = "The device is not in acquisiton mode.";
+      case BITalinoErrorType.DEVICE_NOT_IN_RECORDING_MODE:
+        msg = "The device is not in recording mode.";
         return;
       case BITalinoErrorType.LOST_CONNECTION:
         msg = "The connection to the device was lost.";
@@ -52,11 +58,11 @@ class BITalinoException implements Exception {
         msg =
             "A bluetooth device is already connected. Disconnect that device first.";
         return;
-      case BITalinoErrorType.BT_DEVICE_ALREADY_ACQUIRING:
-        msg = "The bluetooth device is already in acquisiton mode.";
+      case BITalinoErrorType.BT_DEVICE_ALREADY_RECORDING:
+        msg = "The bluetooth device is already in recording mode.";
         return;
-      case BITalinoErrorType.BT_DEVICE_NOT_ACQUIRING:
-        msg = "The bluetooth device is not in acquisiton mode.";
+      case BITalinoErrorType.BT_DEVICE_NOT_RECORDING:
+        msg = "The bluetooth device is not in recording mode.";
         return;
       case BITalinoErrorType.CONTROLLER_FAILED_INITIALIZE:
         msg = "The BITalino controller failed to initialize.";
@@ -76,6 +82,27 @@ class BITalinoException implements Exception {
         return;
       case BITalinoErrorType.NOT_IMPLEMENTED_IOS:
         msg = "This is not implemented for IOS devices";
+        return;
+      case BITalinoErrorType.BAT_THRESHOLD_INVALID:
+        msg =
+            "Invalid value. Parameter threshold must be between 0 (3.4 V) and 63 (3.8 V).";
+        return;
+      case BITalinoErrorType.BT_DEVICE_CANNOT_BE_RECORDING:
+        msg =
+            "BITalino device cannot be recording while perfoming this action.";
+        return;
+      case BITalinoErrorType.INVALID_DIGITAL_CHANNELS:
+        msg =
+            "Invalid digital channels given.\nThe given arary should have a length of 4 with the digital channels to enable set as 1, and the digitalchannels to disable set as 0. If is BITalino 2, the length is 2.";
+        return;
+      case BITalinoErrorType.NOT_BITALINO2:
+        msg = "Device must be BITalino2.";
+        return;
+      case BITalinoErrorType.INVALID_ADDRESS:
+        msg = "The provided MAC address is invalid.";
+        return;
+      case BITalinoErrorType.MISSING_PARAMETER:
+        msg = "This method is missing some parameters.";
         return;
       case BITalinoErrorType.CUSTOM:
         return;
